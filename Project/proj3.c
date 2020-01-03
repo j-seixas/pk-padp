b6cd62cb85e04ec641ca3fce65958c2afa0c9b0b
João Seixas <jnuno1997@gmail.com>
2020-01-03 13:50:09 +0000

    Ex3 from project

---
+++ Project/proj3.c
@@ -0,0 +1,97 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h> 
+#include "mpi.h" 
+
+typedef struct {
+    int upperPart;
+	int factorialPart;
+    int n;
+    int x;
+    double result;
+} Function;
+
+
+int main(int argc, char **argv) {
+	int rank, size, r;
+	int src, dst, tag, i;	
+    int value, stopValue = 20;
+
+	MPI_Status status;
+	MPI_Datatype new_type, type[5] = {MPI_INT, MPI_INT, MPI_INT, MPI_INT, MPI_DOUBLE};
+	int block_lengths_array[5] = {1, 1, 1, 1, 1};
+
+	MPI_Aint disp[5], base, addr;
+	Function func;
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	if(rank == 0) 
+		printf("Creating Function data type for calculations\n");
+
+	MPI_Get_address(&func, &base);
+	MPI_Get_address(&(func.upperPart), &addr);
+	disp[0] = addr - base;
+	MPI_Get_address(&(func.factorialPart), &addr);
+	disp[1] = addr - base;
+    MPI_Get_address(&(func.n), &addr);
+	disp[2] = addr - base;
+    MPI_Get_address(&(func.x), &addr);
+	disp[3] = addr - base;
+    MPI_Get_address(&(func.result), &addr);
+	disp[4] = addr - base;
+	
+	MPI_Type_create_struct(5, block_lengths_array, disp, type, &new_type);
+	MPI_Type_commit(&new_type);
+
+    if(rank != 0) {
+        MPI_Recv(&func, 1, new_type, rank - 1, MPI_ANY_TAG, MPI_COMM_WORLD, &status); 
+        func.n += 1;
+        
+        if (func.x == 0) {
+            func.upperPart = 0;
+            func.factorialPart = 1;
+            func.result = 1;
+
+        } else {
+            func.upperPart *= func.x;
+            func.factorialPart *= func.n;
+
+            double m = func.upperPart / (double) func.factorialPart;
+            func.result += func.upperPart / (double) func.factorialPart;
+        }
+
+        printf("n: %d, result: %f on %d from %d\n", func.n, func.result, rank, status.MPI_SOURCE);
+        MPI_Send(&func, 1, new_type, (rank + 1) % size, 0, MPI_COMM_WORLD);
+    
+    } else {
+        printf("Enter a integer number:\n");
+        scanf("%d", &value);
+        fflush(stdout);
+
+        func.n = 0;
+        func.result = 1;
+		func.upperPart = 1;
+        func.factorialPart = 1;
+        func.x = value;
+        
+        printf("n: %d, result: %f on %d\n", func.n, func.result, rank);
+  
+        //double start = MPI_Wtime();
+		MPI_Send(&func, 1, new_type, (rank + 1) % size, 0, MPI_COMM_WORLD);
+        //double end = MPI_Wtime();
+        //printf("\n\nTime: %lf\n", end - start);
+        if( rank == 0) {
+            MPI_Recv(&func, 1, new_type, size - 1, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            printf("---> Final : %f  Expected : %f\n", func.result, exp(value)); 
+
+        }
+    }
+
+	// MPI_Barrier(MPI_COMM_WORLD);
+	MPI_Finalize();
+	return 0;
+}
+
